72574c44cae3be0c5237e7b833addde81048f6fe
Cocodu34 <corentinp34@gmail.com>
2024-12-02 11:34:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <string>
+#include "Grille.h"
+#include "DisplayGraphique.h"
+#include <limits>
 
+// Nettoie le buffer d'entrée
+void nettoyerBuffer() {
+    while (std::cin.get() != '\n'); // Vide tout le contenu restant dans le buffer
+}
