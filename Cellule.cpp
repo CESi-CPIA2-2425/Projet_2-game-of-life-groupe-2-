b9f483f6196a06fd60413d7e79dded72a2ee2aab
Alebaglivo <126134786+Alebaglivo@users.noreply.github.com>
2024-12-05 09:33:39 +0100

    Update Cellule.cpp

--- Cellule.cpp
+++ Cellule.cpp
@@ -1,1 +1,13 @@
+#include "Cellule.h"
+
+// Calcule le prochain état de la cellule en fonction des règles du Jeu de la Vie
+void Cellule::calculerProchainEtat(int voisinsVivants) {
+    if (estVivante) {
+        // Une cellule vivante reste vivante si elle a 2 ou 3 voisins vivants
+        prochainEtat = (voisinsVivants == 2 || voisinsVivants == 3);
+    } else {
+        // Une cellule morte devient vivante si elle a exactement 3 voisins vivants
+        prochainEtat = (voisinsVivants == 3);
+    }
+}
 
